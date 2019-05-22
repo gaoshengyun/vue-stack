1d42b1f4d1f50b84fd82eec9a58840bba23acf9a
gaoshengyun <18616242909@163.com>
2019-05-22 21:12:54 +0800

    路由实战

---
+++ 2.路由实战/project/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    
+    <button @click="handleClick('back')">返回上一页</button>
+    <button @click="handleClick('push')">跳转到argu</button>
+    <button @click="handleClick('replace')">替换到parent</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    handleClick(type){
+      if(type === 'back'){
+        this.$router.back()
+      }else if(type === 'push'){
+        this.$router.push({
+          name:'argu',
+          params:{
+            name:'gsy'
+          }
+          // query:{
+          //   name:'gsy'
+          // }
+        }) 
+      }else if(type === 'replace'){
+        this.$router.replace({
+          name:'parent'
+        })
+      }
+      
+    }
+  },
+}
+</script>
