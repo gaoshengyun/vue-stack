8f93abc2d6a18e62b539d401a21c3ed8b92920a2
gaoshengyun <18616242909@163.com>
2019-05-24 21:59:19 +0800

    组件之间的通信

---
+++ 4.组件之间的通信/project/src/views/store.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <a-input v-model="inputValue"></a-input>
+    {{inputValue}}
+  </div>
+</template>
+<script>
+import AInput from '../components/AInput'
+export default {
+  name:'store',
+  data() {
+    return {
+      inputValue:''
+    }
+  },
+  components:{
+    AInput
+  }
+}
+</script>
