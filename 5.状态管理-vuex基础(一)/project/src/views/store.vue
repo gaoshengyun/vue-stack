e1f64c9693382413f573f4f5e795a9c2516494d5
gaoshengyun <18616242909@163.com>
2019-05-25 15:46:07 +0800

    状态管理-vuex基础(一)

---
+++ 5.状态管理-vuex基础(一)/project/src/views/store.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <a-input @input="handleInput"></a-input>
+    <p>{{inputValue}}</p>
+    <a-show
+      :content='inputValue'
+    />
+  </div>
+</template>
+<script>
+import AInput from '../components/AInput'
+import AShow from '../components/AShow'
+export default {
+  name:'store',
+  data() {
+    return {
+      inputValue:''
+    }
+  },
+  components:{
+    AInput,
+    AShow
+  },
+  methods: {
+    handleInput(val){
+      this.inputValue = val
+    }
+  },
+}
+</script>
