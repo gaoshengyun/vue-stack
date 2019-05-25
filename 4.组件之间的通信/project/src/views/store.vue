0cbf4aac70dc214e2378775be1bb2435db059e3e
gaoshengyun <18616242909@163.com>
2019-05-25 15:43:14 +0800

    组件之间的通信

--- 4.组件之间的通信/project/src/views/store.vue
+++ 4.组件之间的通信/project/src/views/store.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    <a-input v-model="inputValue"></a-input>
-    {{inputValue}}
+    <a-input @input="handleInput"></a-input>
+    <p>{{inputValue}}</p>
+    <a-show
+      :content='inputValue'
+    />
   </div>
 </template>
 <script>
 import AInput from '../components/AInput'
+import AShow from '../components/AShow'
 export default {
   name:'store',
   data() {
@@ -14,7 +18,13 @@ export default {
     }
   },
   components:{
-    AInput
-  }
+    AInput,
+    AShow
+  },
+  methods: {
+    handleInput(val){
+      this.inputValue = val
+    }
+  },
 }
 </script>
