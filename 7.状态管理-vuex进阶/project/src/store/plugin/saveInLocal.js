2dfee013549b35337bd492da1cb1749e23ea69f5
gaoshengyun <18616242909@163.com>
2019-05-31 15:09:42 +0800

    vuex插件机制

--- 7.状态管理-vuex进阶/project/src/store/plugin/saveInLocal.js
+++ 7.状态管理-vuex进阶/project/src/store/plugin/saveInLocal.js
@@ -1,7 +1,6 @@
 export default store => {
   if(localStorage.state) store.replaceState(JSON.parse(localStorage.state))
     store.subscribe((mutation,state) => {
-      // console.log('提交mutations')
       localStorage.state = JSON.stringify(state)
     })
 }
