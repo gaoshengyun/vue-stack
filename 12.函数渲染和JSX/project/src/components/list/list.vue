7edb70da063484a352c48609f5438a8ad419a4a2
gaoshengyun <18616242909@163.com>
2019-06-05 16:27:47 +0800

    函数式组件

---
+++ 12.函数渲染和JSX/project/src/components/list/list.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in list" :key="`item_${index}`">
+      <span v-if="!render"> {{item.name}} </span>
+      <rendom-dom v-else :render-func="render" :name="item.name"></rendom-dom>
+    </li>
+  </ul>
+</template>
+<script>
+import RendomDom from '../render-dom'
+export default {
+  name:'List',
+  props:{
+    list:{
+      type:Array,
+      default:() => []
+    },
+    render:{
+      type:Function,
+      default:() => {}
+    }
+  },
+  components:{
+    RendomDom
+  }
+}
+</script>
---
+++ 12.函数渲染和JSX/project/src/views/render-page.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <list :list="list" :render="renderFunc"></list>
+  </div>
+</template>
+<script>
+import List from '../components/list'
+export default {
+  data() {
+    return {
+      list:[
+        {name:'gsy'},
+        {name:'gaius'}
+      ]
+    }
+  },
+  components:{
+    List
+  },
+  methods: {
+    renderFunc(h,name){
+      return h('i',{
+        style:{
+          color:'pink'
+        }
+      },name)
+    }
+  },
+}
+</script>
