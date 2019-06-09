14780217c401d4737cb75f7fc297d2895efdcb07
gaoshengyun <18616242909@163.com>
2019-06-09 16:36:41 +0800

    响应式布局

---
+++ 14.响应式布局/project/src/views/split-pane.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="split-pane-con">
+    <split-pane :value.sync="offset">
+      <div slot="left">left</div>
+      <div slot="right">right</div>
+    </split-pane>
+  </div>
+</template>
+<script>
+import SplitPane from '../components/split-pane'
+export default {
+  data() {
+    return {
+      offset:0.8
+    }
+  },
+  components:{
+    SplitPane
+  },methods: {
+    // handleInput(value){
+    //   this.offset = value
+    // }
+  },
+}
+</script>
+<style lang="less" scoped>
+.split-pane-con{
+  width: 400px;
+  height: 200px;
+  background-color: papayawhip
+}
+</style>
